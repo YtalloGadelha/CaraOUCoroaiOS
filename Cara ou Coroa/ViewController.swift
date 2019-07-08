391181cfc80e0ebafd72ea2b1fa1f643982fba8b
Ytallo de Lima Gadelha <ytallolg@gmail.com>
2019-07-08 17:17:21 -0300

    Initial Commit

---
+++ Cara ou Coroa/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cara ou Coroa
+//
+//  Created by Ytallo on 08/07/19.
+//  Copyright © 2019 CursoiOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
